a5ad2231b9a29a63e15065ed971b41b43621b380
cactusboy <13159098@qq.com>
2018-06-17 16:44:34 +0800

    按照工程项目放置web文件

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_URL = '/users/login/'
 
 BOOTSTRAP3 = {
