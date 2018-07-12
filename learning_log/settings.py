0edefe3e9252b816dbf0875c35410937e3ed1205
cactusboy <13159098@qq.com>
2018-07-13 00:52:58 +0800

    none

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z)ta&6lj-_p-d778-pajrxu8e!=iudil2z^5did!u!*ld$z5)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['39.107.80.105', 'localhost', '127.0.0.1']
 
@@ -118,11 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-"""
+
+'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-"""
+'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # 上传图片
